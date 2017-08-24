d6e48adcee115001fea0991a1ac5462bf3bd55a1
Roman Masyhar <mhrohman@live.com>
2017-08-24 19:30:12 +0700

    types(engine): fix wrong path case-sensitive

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,7 +1,7 @@
 import * as Puppeteer from 'puppeteer'
 
 import { readLock } from '../locker'
-import { IEngine, IConfig } from '../types/Engine'
+import { IEngine, IConfig } from '../types/engine'
 
 class Engine implements IEngine {
   public async browser () {
