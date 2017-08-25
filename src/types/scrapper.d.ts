a8757a08d6c47e2139745deb5d847a83dc136376
Roman Masyhar <mhrohman@live.com>
2017-08-25 07:09:29 +0700

    wip(types): add comment doc

--- src/types/scrapper.d.ts
+++ src/types/scrapper.d.ts
@@ -1,22 +1,37 @@
 import { Page } from 'puppeteer'
 
+/**
+ * Data return messages from API.
+ */
 export type TScrapperMessages = {
   from: string
   value: string
   time: string
 }
 
+/**
+ * Data return discussions from API.
+ */
 export type TScrapperDiscussions = TScrapperMessages
 
+/**
+ * Data return notifications from API.
+ */
 export type TScrapperNotification = {
   messages: TScrapperMessages[]
   discussions: TScrapperDiscussions[]
 }
 
+/**
+ * Data return object from API Data key.
+ */
 export type TSCrapperObjectData = {
   notifications: TScrapperNotification
 }
 
+/**
+ * Return object from API.
+ */
 export type TScrapperResult = {
   status: 'success' | 'error'
   message: string
